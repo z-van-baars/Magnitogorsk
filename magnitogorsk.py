076fd1e17c9aac21eb41be503ba74bd871fc271f
Zac <zvanbaars@gmail.com>
2015-12-17 14:24:08 -0800

    added month and yearly iterators.

--- magnitogorsk.py
+++ magnitogorsk.py
@@ -12,7 +12,7 @@ mill = {
     'Rubles': 100
 }
 
-month = [
+months = [
     'January',
     'February',
     'March',
@@ -27,6 +27,8 @@ month = [
     'December'
 ]
 
+month = months[0]
+
 year = 1920
 
 
@@ -35,37 +37,55 @@ class village(object):
     def __init__(self):
         pass
 
-
+# calculates the goal for steel and other conditionals for the monthly turn
 def demand():
     return random.randint(0, 100)
 
 
+# checks production against goal conditions
 def production_check():
 
     if mill['Steel'] >= turn_demand:
         print "You live to toil another day!"
     else:
         print "You have failed the bureaucrats!"
-        break
+
+def workers():
+    pass
+
+# calculates amount of resources consumed and produced per week
+def production():
+    pass
 
 print "MAGNITOGORSk!"
 print "_" * 20
 
+# game loop - iterates 4 * weekly and 12 * monthly with a weekly, monthly, and yearly incrementer
+# calls subfunctions to do production and other game tasks
 while True:
 
+    print "ENTER GLORIOUS COMMUNIST YEAR: %s!" % str(year)
+    month = 0
+    for m in range(12):
+
+
+        turn_demand = demand()
+
+        print "The great leader commands you produce %d tons of steel this month!" % turn_demand
+        for w in range(3):
+            print months[month] + " : " + str(year)
+            print "_" * 80
+            print "Do what?"
+            print "(B)uild // (H)ire and Fire workers // Buy and Sell (R)esources // (E)nd turn"
+            action = raw_input("Do what?")
+            if action == "I":
+                pass
+            elif action == "H":
+                pass
+            elif action == "S":
+                pass
+            elif action == "B":
+                pass
+        raw_input("> ")
+        month += 1
     year += 1
-    turn_demand = demand()
-    print "The great leader commands you produce %d tons of steel this month!" % turn_demand
-    for w in range(3):
-        print "Do what?"
-        print "(B)uild // (H)ire workers // (S)ell resources // (B)uy resources // (E)nd turn"
-        action = raw_input("Do what?")
-        if action == "I":
-            pass
-        elif action == "H":
-            pass
-        elif action == "S":
-            pass
-        elif action == "B":
-            pass
-    raw_input("> ")
